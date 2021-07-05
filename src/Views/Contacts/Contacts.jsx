40a7672bf8f78434adad7b123e0de614d0738c12
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-05 18:04:41 +0300

    api

--- src/Views/Contacts/Contacts.jsx
+++ src/Views/Contacts/Contacts.jsx
@@ -52,12 +52,16 @@ export default function Contacts() {
                   <a
                     href="https://yandex.ru/maps/?from=api-maps&ll=37.558275%2C44.680886&mode=usermaps&origin=jsapi_2_1_78&um=constructor%3A24146f709d6515921f8cfd04efcb22da773672dd494f34f331d8daa625b35c6d&z=16"
                     className="contacts-btn"
+                    target="_blank"
+                    rel="noreferrer noopener"
                   >
                     яндекс карты
                   </a>
                   <a
                     href="https://www.google.ru/maps/dir/%D0%90%D0%B2%D1%82%D0%BE%D0%B2%D0%BE%D0%BA%D0%B7%D0%B0%D0%BB,+%D1%83%D0%BB%D0%B8%D1%86%D0%B0+%D0%A7%D0%B0%D0%B9%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE,+%D0%9D%D0%BE%D0%B2%D0%BE%D1%80%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA/%D0%90%D0%B1%D1%80%D0%B0%D1%83-%D0%94%D1%8E%D1%80%D1%81%D0%BE,+%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80%D1%81%D0%BA%D0%B8%D0%B9+%D0%BA%D1%80%D0%B0%D0%B9,+353995/@44.7330762,37.6225948,12z/data=!4m13!4m12!1m5!1m1!1s0x40f1f93a85a62931:0x1bd1b27d7feca204!2m2!1d37.7627881!2d44.7215967!1m5!1m1!1s0x40f200487f5e1d9f:0xa93584f9dbf3ea68!2m2!1d37.5999358!2d44.7013725"
                     className="contacts-btn"
+                    target="_blank"
+                    rel="noreferrer noopener"
                   >
                     google maps
                   </a>
