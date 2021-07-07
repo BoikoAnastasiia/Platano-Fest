ebc56c9c81777736bcab490643691815dfe0789f
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-07 18:18:02 +0300

    pretty tickets

--- src/Views/BuyTickets/BuyTickets.jsx
+++ src/Views/BuyTickets/BuyTickets.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '../../Сomponents/AppBar';
 import MainContainer from '../../Сomponents/MainContainer';
 import Header from '../../Сomponents/Header';
+import './BuyTickets.scss';
 
 export default function Contacts() {
   return (
@@ -17,15 +18,15 @@ export default function Contacts() {
         <div className="tickets-wrapper">
           <p className="tickets-intro">
             Цены увеличиваются по датам и квотам. На каждую ценовую категорию
-            выделено ограниченное колличество билетов. Как только билеты в
-            категории заканчваются — цена увеличивается.
+            выделено ограниченное количество билетов. Как только билеты в
+            категории заканчиваются&nbsp;— цена увеличивается.
           </p>
           <ul className="tickets-list">
             <li className="tickets-list-item">
               <div className="left-side-ticket">
                 <p className="ticket-date">2 июня / дюрсо</p>
                 <div className="middle-left-ticket-wrapper">
-                  <h2 className="ticket-header">базовый один день</h2>
+                  <h2 className="ticket-header">базовый один&nbsp;день</h2>
                   <p className="ticket-descr">
                     кемпинг / 1 мастер-класс / 1 квест
                   </p>
@@ -41,6 +42,68 @@ export default function Contacts() {
                 <p className="buy-text-active">ДО 30.05 — 1 000 ₽</p>
                 <p className="buy-text-passive">ДО 30.05 — 1 000 ₽</p>
                 <p className="buy-text-passive">ДО 30.05 — 1 000 ₽</p>
+              </div>
+            </li>
+            <li className="tickets-list-item">
+              <div className="left-side-ticket">
+                <p className="ticket-date">2 июня / дюрсо</p>
+                <div className="middle-left-ticket-wrapper">
+                  <h2 className="ticket-header">ПОЛНЫЙ ОДИН&nbsp;ДЕНЬ</h2>
+                  <p className="ticket-descr">
+                    ХОСТЕЛ / 2 МАСТЕР-КЛАССА / 1 КВЕСТ
+                  </p>
+                </div>
+                <p className="secondary-ticket-descr">* ВКЛЮЧЕН КЕМПИНГ</p>
+              </div>
+              <div className="right-side-ticket">
+                <a href="https://platanos.ru/#tickets" className="tickets-btn ">
+                  купить билеты
+                </a>
+                <p className="buy-text-active">ДО 30.05 — 2 000 ₽</p>
+                <p className="buy-text-passive">ДО 30.05 — 2 500 ₽</p>
+                <p className="buy-text-passive">ДО 30.05 — 3 500 ₽</p>
+              </div>
+            </li>
+            <li className="tickets-list-item">
+              <div className="left-side-ticket">
+                <p className="ticket-date">2—3 ИЮНЯ / ДЮРСО</p>
+                <div className="middle-left-ticket-wrapper">
+                  <h2 className="ticket-header">базовый два&nbsp;дня</h2>
+                  <p className="ticket-descr">
+                    КЕМПИНГ / 3 МАСТЕР-КЛАССА / 1 КВЕСТ
+                  </p>
+                </div>
+                <p className="secondary-ticket-descr">
+                  *возможна аренда палатки
+                </p>
+              </div>
+              <div className="right-side-ticket">
+                <a href="https://platanos.ru/#tickets" className="tickets-btn ">
+                  купить билеты
+                </a>
+                <p className="buy-text-active">ДО 30.05 — 2 000 ₽ ₽</p>
+                <p className="buy-text-passive">ДО ДО 15.06 — 2 500 ₽</p>
+                <p className="buy-text-passive">ДО 2.07 — 3 000 ₽</p>
+              </div>
+            </li>
+            <li className="tickets-list-item">
+              <div className="left-side-ticket">
+                <p className="ticket-date">2—3 ИЮНЯ / ДЮРСО</p>
+                <div className="middle-left-ticket-wrapper">
+                  <h2 className="ticket-header">базовый один&nbsp;день</h2>
+                  <p className="ticket-descr">
+                    кемпинг / 1 мастер-класс / 1 квест
+                  </p>
+                </div>
+                <p className="secondary-ticket-descr">* ВКЛЮЧЕН КЕМПИНГ</p>
+              </div>
+              <div className="right-side-ticket">
+                <a href="https://platanos.ru/#tickets" className="tickets-btn ">
+                  купить билеты
+                </a>
+                <p className="buy-text-active">ДО 30.05 — 3 000 ₽</p>
+                <p className="buy-text-passive">ДО 15.06 — 3 500 ₽</p>
+                <p className="buy-text-passive">ДО 2.07 — 4 000 ₽</p>
               </div>
             </li>
           </ul>
